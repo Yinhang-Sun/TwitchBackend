fba02b813efa1bf98cf1d2e9a08b4f65cbd3e64d
yinhang_sun <sunyinhang@gmail.com>
2023-01-06 12:53:15 -0800

    first commit

---
+++ src/main/java/com/laioffer/jupiter/entity/db/ItemType.java
@@ -0,0 +1,12 @@
+package com.laioffer.jupiter.entity.db;
+
+public enum ItemType {
+    STREAM, VIDEO, CLIP
+}
+
+
+//package com.laioffer.jupiter.entity.db;
+//
+//public enum ItemType {
+//    STREAM, VIDEO, CLIP
+//}
---
+++ src/main/java/com/laioffer/jupiter/service/RecommendationException.java
@@ -0,0 +1,17 @@
+package com.laioffer.jupiter.service;
+
+public class RecommendationException extends RuntimeException {
+    public RecommendationException(String errorMessage) {
+        super(errorMessage);
+    }
+}
+
+
+
+//package com.laioffer.jupiter.service;
+//
+//public class RecommendationException extends RuntimeException {
+//    public RecommendationException(String errorMessage) {
+//        super(errorMessage);
+//    }
+//}
