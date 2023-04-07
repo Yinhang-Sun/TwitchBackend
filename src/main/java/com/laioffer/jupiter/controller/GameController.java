e9b0cc3647ed46c7ac4cb0243d9bce918e877f56
yinhang_sun <sunyinhang@gmail.com>
2023-04-06 22:27:47 -0700

    implement Favorite

--- src/main/java/com/laioffer/jupiter/controller/GameController.java
+++ src/main/java/com/laioffer/jupiter/controller/GameController.java
@@ -13,6 +13,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+
+// test change 5
 @Controller
 public class GameController {
 
