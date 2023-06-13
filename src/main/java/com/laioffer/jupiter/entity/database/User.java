f731eb81ffc0b3a1df3a2218ff522c07fdca7e29
yinhang_sun <sunyinhang@gmail.com>
2023-06-13 11:14:51 -0700

    implement hibernate

---
+++ src/main/java/com/laioffer/jupiter/entity/database/User.java
@@ -0,0 +1,67 @@
+package com.laioffer.jupiter.entity.database;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "user")
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @Column(name = "user_id")
+    @JsonProperty("user_id")
+    private String userId;
+
+    @JsonProperty("password")
+    private String password;
+
+    @Column(name = "first_name")
+    @JsonProperty("first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    @JsonProperty("last_name")
+    private String lastName;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
